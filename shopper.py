41e982872e01a4041abfd84946dc2b8c718f09e0
panupongdeve <panupong.dev.web@gmail.com>
2023-04-09 01:28:54 +0000

    Add Resource Detector

--- shopper.py
+++ shopper.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python3
 
 from opentelemetry import context, trace
+from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor, BatchSpanProcessor
 
-def configure_tracer():
+from local_machine_resource_detector import LocalMachineResourceDetector
+
+def configure_tracer(name, version):
     exporter = ConsoleSpanExporter()
-    span_processor = SimpleSpanProcessor(exporter)
-    provider = TracerProvider()
+    span_processor = BatchSpanProcessor(exporter)
+    local_resource = LocalMachineResourceDetector().detect()
+    resource = local_resource.merge(
+        Resource.create(
+            {
+                "service.name": name,
+                "service.version": version
+            }
+        )
+    )
+    provider = TracerProvider(resource=resource)
     provider.add_span_processor(span_processor)
     trace.set_tracer_provider(provider)
-    return trace.get_tracer("shopper.py", "0.0.1")
+    return trace.get_tracer(name, version)
 
-tracer = configure_tracer()
+tracer = configure_tracer("shopper", "0.1.2")
 
 @tracer.start_as_current_span("browse")
 def browse():
